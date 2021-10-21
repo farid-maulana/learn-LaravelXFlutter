ef9b6371cc150d1aee2b0c86e11487964f06d083
farid-maulana <faridmaulana1921@gmail.com>
2021-10-21 23:38:30 +0700

    CRUD transactions

---
+++ flutter-api/app/Http/Controllers/TransactionController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreTransactionRequest;
+use App\Http\Resources\TransactionResource;
+use App\Models\Transaction;
+use Illuminate\Http\Request;
+
+class TransactionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return TransactionResource::collection(Transaction::with('category')->get());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreTransactionRequest $request)
+    {
+        return new TransactionResource(Transaction::create($request->validated()));
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Transaction  $transaction
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Transaction $transaction)
+    {
+        return new TransactionResource($transaction);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Transaction  $transaction
+     * @return \Illuminate\Http\Response
+     */
+    public function update(StoreTransactionRequest $request, Transaction $transaction)
+    {
+        $transaction->update($request->validated());
+
+        return new TransactionResource($transaction);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Transaction  $transaction
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Transaction $transaction)
+    {
+        $transaction->delete();
+
+        return response()->noContent();
+    }
+}
