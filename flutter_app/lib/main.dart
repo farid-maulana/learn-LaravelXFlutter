4f23e729024ebb5e0b3640b99b25dc31d3214311
farid-maulana <faridmaulana1921@gmail.com>
2021-11-07 21:59:06 +0700

    Login form

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,26 +1,42 @@
+// ignore_for_file: prefer_const_constructors
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Welcome to Flutter',
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Welcome to Flutter'),
-        ),
-        body: Center(
-          child: Text(
-            'Hello World',
-            style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-            ),
+          appBar: AppBar(
+            title: Text('Login'),
           ),
-        ),
-      ),
+          body: Column(
+            children: [
+              TextField(
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  labelText: 'Email',
+                ),
+              ),
+              TextField(
+                keyboardType: TextInputType.visiblePassword,
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () => print('Login clicked'),
+                child: Text('Login'),
+                style: ElevatedButton.styleFrom(
+                  minimumSize: Size(double.infinity, 36),
+                ),
+              )
+            ],
+          )),
     );
   }
 }
